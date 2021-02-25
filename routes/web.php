afd666b06037eb27c2ac0a7c5c08bec94badc1b9
ChamathkaDissanayaka <chamathka.dissa@gmail.com>
2021-02-25 20:30:13 +0530

    added login page and logout functionalities

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('auth.login');
+});
+
+Route::get('index', function () {
     return view('index');
 });
 
